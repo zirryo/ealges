3cb1763df8edf9fee6024b138b149a5a71841413
zirryo <jh04187@gmail.com>
2024-07-03 13:47:13 +0900

    :sparkles: 비즈니스 예외 및 하위 클래스 구현 #4

---
+++ server/src/main/java/com/server/global/error/exception/BusinessException.java
@@ -0,0 +1,20 @@
+package com.server.global.error.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
---
+++ server/src/main/java/com/server/global/error/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.server.global.error.exception;
+
+public class EntityNotFoundException extends BusinessException {
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.ENTITY_NOT_FOUND);
+    }
+
+}
