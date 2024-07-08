38e05157ee34e97d82e0fb594f4b3960607dc57a
zirryo <jh04187@gmail.com>
2024-07-08 17:57:31 +0900

    :sparkles: Seat 엔티티 구현 #5

---
+++ server/src/main/java/com/server/domain/seat/entity/Seat.java
@@ -0,0 +1,43 @@
+package com.server.domain.seat.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.server.domain.game.entity.Game;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seatId;
+
+    @Column(nullable = false)
+    private Integer seatLogicalId;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne
+    @JoinColumn(name = "gameId", nullable = false, updatable = false)
+    private Game game;
+
+    @Column(nullable = false)
+    private String blockName;
+
+    @Column(nullable = false)
+    private boolean isAvailable;
+
+    @Builder
+    public Seat(Integer seatLogicalId, Game game, String blockName) {
+        this.seatLogicalId = seatLogicalId;
+        this.game = game;
+        this.blockName = blockName;
+        this.isAvailable = true;
+    }
+
+}
